18c24588e6fb3c75eeaca176223aaa1278580027
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2021-06-21 07:50:01 +0200

    exemple WPF

---
+++ Chap8/desktop/wpf/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows;
+
+namespace DesktopWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Text = "Dernière fois que le bouton a été cliqué : " + DateTime.Now;
+            MessageBox.Show("Vous avez cliqué sur le bouton");
+        }
+    }
+}
