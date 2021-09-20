3e96ee6e7fa5604e4ee3e8e99adafecaea6d9a80
Christophe MOMMER <hybrid-technologies-solutions@outlook.com>
2021-09-20 07:37:48 +0200

    Migration .NET 6

--- Chap3/ExerciceGarage/Program.cs
+++ Chap3/ExerciceGarage/Program.cs
@@ -1,19 +1,10 @@
-﻿using System;
+﻿using ExerciceGarage;
 
-namespace ExerciceGarage
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            var garage = new Garage();
-            var peugeot = new Voiture { Marque = "Peugeot", Couleur = Couleur.Bleu };
-            var ferrari = new Voiture { Marque = "Ferrari", Couleur = Couleur.Rouge };
-            var camion = new Camion();
-            garage.Reparer(camion);
-            garage.Reparer(peugeot);
-            garage.FaireEntretien(ferrari);
-            garage.Repeindre(peugeot, Couleur.Vert);
-        }
-    }
-}
+var garage = new Garage();
+var peugeot = new Voiture { Marque = "Peugeot", Couleur = Couleur.Bleu };
+var ferrari = new Voiture { Marque = "Ferrari", Couleur = Couleur.Rouge };
+var camion = new Camion();
+garage.Reparer(camion);
+garage.Reparer(peugeot);
+garage.FaireEntretien(ferrari);
+garage.Repeindre(peugeot, Couleur.Vert);
