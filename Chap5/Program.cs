5f3079a219511a118acba415b76d5bfaaace0654
Christophe MOMMER <christophe.mommer@live.fr>
2023-03-31 08:33:37 +0200

    mise à jour vers .NET 8

--- Chap5/Program.cs
+++ Chap5/Program.cs
@@ -1,29 +1,28 @@
-﻿using System;
-using System.Linq;
-
-var entiers = Enumerable.Range(1, 100);
-var divisibleParSept = entiers.Where(e => e % 7 == 0);
-var divisibleParSeptInline = from e in entiers where e % 7 == 0 select e;
-
-foreach (var item in divisibleParSept)
-{
-    Console.Write("\t" + item);
-}
-Console.WriteLine();
-foreach (var item in divisibleParSeptInline)
-{
-    Console.Write("\t" + item);
-}
-Console.WriteLine();
-
-var groups = divisibleParSept.GroupBy(e => e % 2).Select(g => new { Nom = g.Key == 0 ? "Pair" : "Impair", Nombres = g.ToList() });
-
-foreach (var item in groups)
-{
-    Console.WriteLine(item.Nom);
-    foreach (var value in item.Nombres)
-    {
-        Console.WriteLine("\t" + value);
-    }
-    Console.WriteLine();
+﻿var entiers = Enumerable.Range(1, 100); 
+var divisibleParSept = entiers.Where(e => e % 7 == 0); 
+var divisibleParSeptInline = from e in entiers where e % 7 == 0 select e; 
+ 
+foreach (var item in divisibleParSept) 
+{ 
+    Console.Write("\t" + item); 
+} 
+Console.WriteLine(); 
+foreach (var item in divisibleParSeptInline) 
+{ 
+    Console.Write("\t" + item); 
+} 
+Console.WriteLine(); 
+ 
+var groups = divisibleParSept
+    .GroupBy(e => e % 2)
+    .Select(g => new  { Nom = g.Key == 0 ? "Pair" : "Impair", Nombres = g.ToList() }); 
+ 
+foreach (var item in groups) 
+{ 
+    Console.WriteLine(item.Nom); 
+    foreach (var value in item.Nombres) 
+    { 
+        Console.WriteLine("\t" + value); 
+    } 
+    Console.WriteLine(); 
 }
